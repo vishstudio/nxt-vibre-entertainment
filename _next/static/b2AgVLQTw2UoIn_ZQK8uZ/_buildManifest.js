390dc5a0288bc24bf65f0439fccc6c81babadde5
VishSeen <VishSeen@users.noreply.github.com>
2026-03-16 18:15:15 +0000

    deploy: 6bcc2152ea508b1acbedfb36fefe3242da7ccd08

---
+++ _next/static/b2AgVLQTw2UoIn_ZQK8uZ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nxt-vibre-entertainment//_next/:path+",
+        "destination": "/nxt-vibre-entertainment/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
